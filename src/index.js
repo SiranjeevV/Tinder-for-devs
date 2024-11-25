2336aa196af7bb859d59bf7ae24aaa0243ec8246
SiranjeevV <siranjeev.btech@gmail.com>
2024-11-25 17:26:57 +0530

    creates a server

--- src/index.js
+++ src/index.js
@@ -2,14 +2,42 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
+app.post("/", (req, res) => {
     res.send("hello from server");
 });
-app.get("/app", (req, res) => {
+
+//LOCALHOST:7777/app/101
+
+app.get("/app/:userId", (req, res) => {
+    console.log(req.params);  // {userId:101}
     res.send("hello from app");
 });
 
-app.get("/test", (req, res) => {
+//LOCALHOST:7777/app?id=101
+
+// app.get("/app", (req, res) => {
+//     console.log(req.query); // {id:101}
+//     res.send("hello from app");
+// });
+
+//B IS OPTIONAL
+// app.use("/ab?c", (req, res) => {
+//     res.send("hello from test");
+// });
+
+//CAN ADD MULTIPLE B
+// app.use("/ab+c", (req, res) => {
+//     res.send("hello from test");
+// });
+
+//ANYTHING BETWEEN B AND C
+// app.use("/ab*cd", (req, res) => {
+//     res.send("hello from test");
+// });
+
+
+//ANY PATHS AFTER A => A/AB/AB IS WORKS
+app.use("/a/", (req, res) => {
     res.send("hello from test");
 });
 
